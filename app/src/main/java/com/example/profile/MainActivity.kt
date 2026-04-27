4684272a3520cca2a9afd7aa4e91d875b2dad587
rezaokta-ui <User1592024@hotmail.com>
2026-04-27 19:07:45 +0700

    update lagi cuy

--- app/src/main/java/com/example/profile/MainActivity.kt
+++ app/src/main/java/com/example/profile/MainActivity.kt
@@ -6,15 +6,25 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+//import library
+import androidx.widget.Button
+import androidx.content.Intent
+
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val btnNext = findViewById<Button>(id = R.i.dbtnLanjut)
+        // membuat fungsi klik pada button
+        btnNext.aet0nClicklistener {
+            val intent = Intent( packageConext = this, cls = Profile2::class.java)
+            startActivity(intent)
+        }
+
         }
     }
-}
